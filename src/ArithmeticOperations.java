5bd7f6daa7271036d4bc04364835fff0732e4a5d
iikot <iikotoarusa11@gmail.com>
2023-11-13 22:52:52 +0900

    Javaプログラミング入門学習終了

---
+++ src/ArithmeticOperations.java
@@ -0,0 +1,25 @@
+
+public class ArithmeticOperations {
+
+    public static void main(String[] args) {
+        // 変数の宣言・初期化
+        int num1 = 100;
+        int num2 = 10;
+
+        int result = num1 + num2;
+        System.out.println("num1+num2="+result);
+
+        int result = num1 - num2;
+        System.out.println("num1-num2="+result);
+
+        int result = num1 * num2;
+        System.out.println("num1*num2="+result);
+
+        int result = num1 /num2;
+        System.out.println("nun1/num2="+result);
+
+        int result = num1 % num2;
+        System.out.println("num1%num2="+result);
+    }
+
+}
